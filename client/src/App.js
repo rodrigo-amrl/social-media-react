770549b8ac61dc53d150d5488e6cef021e3acd11
Rodrigo Amaral <rodrigo.amaral94@hotmail.com>
2024-10-24 18:34:30 -0300

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,25 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import loginPage from "scenes/loginPage";
 import profilePage from "scenes/profilePage";
-
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
+import { themeSettings } from "theme";
 function App() {
+  const mode = useSelector((state) => state.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<loginPage />}></Route>
-          <Route path="/home" element={<HomePage />}></Route>
-          <Route path="/profile/:userId" element={<profilePage />}></Route>
-        </Routes>
+        <ThemeProvider>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<loginPage />}></Route>
+            <Route path="/home" element={<HomePage />}></Route>
+            <Route path="/profile/:userId" element={<profilePage />}></Route>
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
