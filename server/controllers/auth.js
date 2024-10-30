023930e91ed7022ff8984bdbfe56e211e2be5b85
Rodrigo Amaral <rodrigo.amaral94@hotmail.com>
2024-10-30 18:15:19 -0300

    update server e package

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -40,7 +40,6 @@ export const register = async (req, res) => {
 
 /* LOGGING IN */
 export const login = async (req, res) => {
-  console.log('asdfasdf')
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email: email });
