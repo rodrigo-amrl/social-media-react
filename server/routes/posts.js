cbefa476e11eee7efd00bbe37c59a93444bc6702
Rodrigo Amaral <rodrigo.amaral94@hotmail.com>
2024-10-30 17:00:48 -0300

    ajuste server

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,13 +1,14 @@
-import express from "express"
-import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js"
-import { verifyToken } from "../middleware/auth.js"
-import { getUser } from "../controllers/users";
+import express from "express";
+import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/", verifyToken, getFeedPosts)
-router.get("/:userId,posts", verifyToken, getUserPosts)
+/* READ */
+router.get("/", verifyToken, getFeedPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
 
-router.patch("/:id/like", verifyToken, likePost)
+/* UPDATE */
+router.patch("/:id/like", verifyToken, likePost);
 
 export default router;
